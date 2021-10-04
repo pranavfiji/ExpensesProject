0945141e23b00f7d97da8ad462d5b8a64eeb7b25
pranavfiji <69599542+pranavfiji@users.noreply.github.com>
2021-10-04 18:08:04 +1200

    Add files via upload

---
+++ App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {StyleSheet,Text,View,Image} from 'react-native'
+ 
+export default class App extends React.Component{
+    render(){
+        return(
+            <View>
+                <Text>Hi</Text>
+            </View>
+        );
+    }
+}
---
+++ expenseTracker.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import {StyleSheet,Text,View,Image} from 'react-native'
+
+export default class ExpenseMonitor extends React.Component{
+    
+}
